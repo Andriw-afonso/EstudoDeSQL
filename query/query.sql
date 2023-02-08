64ca86bd85c131f9904da2abc71a310733eff037
Andriw-afonso <andriw_eng@outlook.com>
2023-02-08 14:18:33 -0300

    nlknlkn

--- query/query.sql
+++ query/query.sql
@@ -231,13 +231,76 @@ WHERE id = 11 ;
 -- Altere as compras, colocando a observação 'preparando o natal' para todas as que foram efetuadas no dia 20/12/2014.
 UPDATE compras SET observacoes = 'preparandodo o natal' WHERE data =  '2014/12/20';
 
-select*
+-- Altere o VALOR das compras feitas antes de 01/06/2013. Some R$10,00 ao valor atual.
+UPDATE compras 
+SET valor = valor + 10 
+WHERE data < '2013/06/01';
+
+
+-- Atualize todas as compras feitas entre 01/07/2013 e 01/07/2014 para que elas tenham a observação
+--' entregue antes de 2014' e a coluna recebida com o valor TRUE.
+UPDATE compras
+SET observacoes = 'entregue antes de 2014' , recebida = TRUE
+WHERE data BETWEEN '2013/07/01' AND '2014/07/01';
+
+-- Em um comando WHERE é possível especificar um intervalo de valores. Para tanto, é preciso dizer
+-- qual o valor mínimo e o valor máximo que define o intervalo. Qual é o operador que é usado para
+-- isso?
+SELECT*
 FROM compras
-where data = '2014/12/20';
+WHERE valor BETWEEN 100 AND 200;
 
+-- Qual operador você usa para remover linhas de compras de sua tabela?
+DELETE 
+FROM compras
+WHERE data = '2012/12/15';
 
+-- Exclua as compras realizadas entre 05 e 20 março de 2013.
+DELETE
+FROM compras
+WHERE data BETWEEN '2013/03/05' AND '2013/03/20';
 
+-- Use o operador NOT e monte um SELECT que retorna todas as compras com valor diferente de R$108,00.
+SELECT *
+FROM compras
+WHERE NOT valor = 108 ;
 
+-- Selecionando observacoes nula
+SELECT*
+FROM compras
+WHERE observacoes IS NULL;
 
+-- Alterando a coluna observacoes para não aceitar valores nulos.
+ALTER TABLE compras MODIFY COLUMN observacoes VARCHAR(255) NOT NULL;
 
+-- Testando se a colunas observacoes aceita valores nulos
+INSERT INTO compras (valor, data, recebida, observacoes)
+VALUES ( 150, '2016-01-04', 1 , NULL )
+
+-- Inserindo o valor default na coluna recebida
+ALTER TABLE compras MODIFY COLUMN recebida TINYINT(1) DEFAULT 0;
+
+INSERT INTO compras (valor, data, observacoes)
+VALUES (150, '2016-01-05', 'Compra de teste');
+
+-- Testando o valor default na coluna recebida
+INSERT INTO compras (valor, data, observacoes)
+VALUES (150, '2016-01-05', 'Compra de teste');
+SELECT*
+FROM compras;
+
+-- Configure o valor padrão para a coluna recebida .
+ALTER TABLE compras MODIFY COLUMN recebida TINYINT(1) DEFAULT 0;
+
+-- Configure a coluna observacoes para não aceitar valores nulos.
+ALTER TABLE compras MODIFY COLUMN observacoes VARCHAR(255) NOT NULL;
+
+-- NOT NULL e DEFAULT podem ser usados também no CREATE TABLE ? Crie uma tabela nova e
+--adicione Constraints e valores DAFAULT .
+CREATE TABLE carro(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    carro VARCHAR(255) NOT NULL,
+    rodas INT DEFAULT 4,
+    arcondicionado TINYINT DEFAULT 0
+);
 
